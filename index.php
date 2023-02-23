8639ae2a3e222ccd5cb76d1f90586e336b0757dc
IgorFilin <filinigor@yandex.ru>
2023-02-23 20:01:33 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$int = 10;
+$float = 1.22;
+$str = 'Hello';
+$str2 = 'World';
+$str3 = $str . ' ' . $str2;
+$array = [1,2,3,4];
+$null = null;
+
+print_r($array) ; // как console.log
+var_dump($float); // как typeof
