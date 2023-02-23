2fb095ca443e0db311f630dbfe09c312e24eea0c
IgorFilin <filinigor@yandex.ru>
2023-02-23 21:46:20 +0300

    feat

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 $int = 10;
 $float = 1.22;
-$str = 'Hello';
+$str = 'hello';
 $str2 = 'World';
 $str3 = $str . ' ' . $str2;
 $array = [1,2,3,4];
 $null = null;
+$array2= ['1' => 22];
+$upperFirstItem = ucfirst($str);
 
-print_r($array) ; // как console.log
-var_dump($float); // как typeof
+function Foo ($value) {
+   return $value + 22;
+};
+
+// print_r() ; // как console.log
+// var_dump(); // как typeof
+
+print_r(Foo($int)) ;
+print_r($upperFirstItem) ;
