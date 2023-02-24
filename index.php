02c495e1ea266f003a037f6c775d893364e173be
IgorFilin <filinigor@yandex.ru>
2023-02-24 14:43:16 +0300

    feat/map,filter,reduce and training debug

--- index.php
+++ index.php
@@ -1,20 +1,49 @@
 <?php
-$int = 10;
-$float = 1.22;
-$str = 'hello';
-$str2 = 'World';
-$str3 = $str . ' ' . $str2;
-$array = [1,2,3,4];
-$null = null;
-$array2= ['1' => 22];
-$upperFirstItem = ucfirst($str);
+// $int = 10;
+// $float = 1.22;
+// $str = 'hello';
+// $str2 = 'World';
+// $str3 = $str . ' ' . $str2;
+// $array = [1,2,3,4];
+// $null = null;
+// $array2= ['1' => 22];
+// $upperFirstItem = ucfirst($str);
 
-function Foo ($value) {
-   return $value + 22;
-};
+// function Foo ($value) {
+//    return $value + 22;
+// };
 
 // print_r() ; // как console.log
 // var_dump(); // как typeof
 
-print_r(Foo($int)) ;
-print_r($upperFirstItem) ;
+// $replaceStr = str_replace('d','dd',$str2); // заменить в строке нужные символы
+
+// print_r(Foo($int));
+// print_r($upperFirstItem) ;
+// print_r($replaceStr);
+
+//===============================================================================================================================
+
+$testArray = [['id'=> 1,'name'=> 'Igor','age'=> 28],['id'=> 2,'name'=> 'Alisa','age'=> 23],['id'=> 3,'name'=> 'Bob','age'=> 25]];
+
+//==================================
+function ageIncrementMap ($el){
+   return [...$el,'age' => $el['age'] + 1];
+}; // map
+
+function filterArray ($el){
+   return $el['id'] === 2;
+}; // filter
+
+function reduceArray ($acc,$curr){
+    array_push($acc,$curr['name']);
+    return $acc;
+}; // reduce
+
+//==================================
+
+// $newArray = array_filter($testArray,"ageIncrement");
+// $newArray = array_map("ageIncrementMap",$testArray);
+$newArray = array_reduce($testArray,"reduceArray",[]);
+
+print_r($newArray);
