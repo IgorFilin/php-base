f402622ffffb36f975583543bbff8a35525210ff
IgorFilin <filinigor@yandex.ru>
2023-03-08 16:29:45 +0300

    fix(2)

--- web-test-component-array.php
+++ web-test-component-array.php
@@ -4,4 +4,9 @@ $array = [
     ['id' => 1, 'name' => 'Alisa',  'age' => 25],
     ['id' => 1, 'name' => 'Bob',  'age' => 28]
 ];
+
+for($i = 0;$i < count($array);$i++) {
+    $temp = $array[$i]['name'];
+      echo "<li>$temp</li>";
+};
 ?>
